e25bca7e563867176b3d2a0138f6b9f36d885960
SOLODILLYA <solodillya@gmail.com>
2025-07-24 10:36:15 +0200

    Login Page Created
    Created a Log In page

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import LoginPage from "./components/Login";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/sumup/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </Router>
   );
---
+++ src/main/frontend/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Login.css"; // Adjust path as needed
+
+const LoginPage = () => {
+  const handleLogin = () => {
+    window.location.href = "https://web.sumup.com/login";
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-card">
+        <h1 className="login-title">SumUp Dashboard</h1>
+        <button className="login-button" onClick={handleLogin}>
+          Login using SumUp
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
